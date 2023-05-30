31b27a95fdf0eded2c1257f6f301e8d3605cbbb9
karthik <karthikwadeyar45@gmail.com>
2023-05-30 19:57:53 +0530

    First commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react'
+import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+
+
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch('https://food-app-25816-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json')
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!!');
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        })
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    }
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    })
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    )
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      price={meal.price}
+      description={meal.description}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  )
+}
+
+export default AvailableMeals
